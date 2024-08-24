326a3c9749563472966a4d8c74c197d7751ae39e
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-08-24 22:09:06 +0530

    sked ques-leetcode 84

--- index.js
+++ index.js
@@ -44,8 +44,35 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
       },
     ],
   });
-  let result = await chat.sendMessage("How you calculated that?");
+
+  let ques=`84. Largest Rectangle in Histogram
+Hard
+Topics
+Companies
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104`;
+  let result = await chat.sendMessage(ques);
   console.log(result.response.text());
-  result = await chat.sendMessage("Man i killed one of them out of anger! What to do?");
-  console.log(result.response.text());
+  // result = await chat.sendMessage("Man i killed one of them out of anger! What to do?");
+  // console.log(result.response.text());
   
